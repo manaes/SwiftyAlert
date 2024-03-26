736c80f5cdd4ee79f2146eec1e4df5d6edb4f415
wanny.park <wanny.park@pandora.tv>
2024-03-26 17:31:41 +0900

    Carthage 지원 추가

---
+++ Sources/SwiftyAlert/SwiftyAlert.h
@@ -0,0 +1,18 @@
+//
+//  SwiftyAlert.h
+//  SwiftyAlert
+//
+//  Created by manaes on 3/26/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SwiftyAlert.
+FOUNDATION_EXPORT double SwiftyAlertVersionNumber;
+
+//! Project version string for SwiftyAlert.
+FOUNDATION_EXPORT const unsigned char SwiftyAlertVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftyAlert/PublicHeader.h>
+
+
